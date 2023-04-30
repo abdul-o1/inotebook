1343dbd7e770b961b608fc9775441381d5a0abb1
Abdul Rahman <rahman567123@gmail.com>
2023-04-30 11:02:22 +0100

    added a module to add a note and also fetch the added note

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,27 +1,34 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
 const NotesSchema = new Schema(
     {
+        user:{
+            type : mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+
         title:{
-            type : string,
+            type : String,
             required : true,
 
         },
         
         description:{
-            type : string,
+            type : String,
             required : true,
 
         } ,
 
         tag:{
-            type : string,
+            type : String,
             default: 'general'
 
         },
         date:
         {
-            type: string,
+            type: String,
             default: Date.now,
         }
 });
- module.exports = mongoose.models('notes',NotesSchema );
+ module.exports = mongoose.model('notes',NotesSchema );
