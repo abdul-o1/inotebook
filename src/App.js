98c1b7a08b27c888877c4e99dada79c288d0e837
Abdul Rahman <rahman567123@gmail.com>
2023-05-17 12:34:14 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/NoteState";
+import { Alert } from "./components/Alert";
 
 function App() {
   return (
     <>
       <NoteState>
+
         <Router>
           <Navbar />
+        <Alert message="This is amazing React course" />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
--- src/components/Note.js
+++ src/components/Note.js
@@ -8,8 +8,8 @@ const Notes = () => {
     const context = useContext(NoteContext);
     const {notes, setNotes} = context;
     return (
-        <div className="row my-3">
-            <h2>You Notes</h2> 
+        <div className=" row my-3">
+            <h2>Your Notes</h2> 
             {notes.map((note)=>{
                 return <Noteitem key={note._id} note={note}/>  
             })}
