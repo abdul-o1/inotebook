0492a174569ad1f17a8abd3d6b0a57d8b0ea1155
Abdul Rahman <rahman567123@gmail.com>
2023-05-17 11:56:37 +0100

    just added some minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
- } from "react-router-dom";
- import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import NoteState from './context/NoteState';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/NoteState";
 
 function App() {
   return (
     <>
-    <NoteState>
-       <Router>
-        <Navbar /> 
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
       </NoteState>
     </>
   );
