1749efd55056a656a500e113d350b73f15446c55
Abdul Rahman <rahman567123@gmail.com>
2023-05-18 12:19:05 +0100

    added a functionality to add notes on the client side

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,18 +2,21 @@ import React from "react";
 import NoteContext from "../context/NoteContext";
 import { useContext } from "react";
 import Noteitem from "./Noteitem";
-
+import AddNote from "./AddNote";
 
 const Notes = () => {
     const context = useContext(NoteContext);
-    const {notes, setNotes} = context;
+    const {notes, addNote} = context;
     return (
-        <div className=" row my-3">
-            <h2>Your Notes</h2> 
+        <>
+        <AddNote/>
+        <div className="row my-3">
+            <h2>You Notes</h2> 
             {notes.map((note)=>{
                 return <Noteitem key={note._id} note={note}/>  
             })}
             </div>
+            </>
     )
 }
 
