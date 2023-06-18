b335163aadac3874f19b18ade9f84900aec531de
Abdul Rahman <rahman567123@gmail.com>
2023-06-18 11:45:11 +0100

    create a login and signup page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/NoteState";
 import { Alert } from "./components/Alert";
-
+import Login from "./components/Login"
+import SignUp from "./components/SignUp";
 function App() {
   return (
     <>
       <NoteState>
-
         <Router>
           <Navbar />
-        <Alert message="This is amazing React course" />
+          <Alert message="This is amazing React course" />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<SignUp />} />
+              
             </Routes>
           </div>
         </Router>
@@ -25,5 +28,6 @@ function App() {
     </>
   );
 }
+
 
 export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      this sign up
+    </div>
+  )
+}
+
+export default SignUp
